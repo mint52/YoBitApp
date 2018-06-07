088dbe334343c6c0ac4dbd45ed9e0b6ae2411d1c
Kolyan <leo.sparrow@yandex.ru>
2018-06-07 16:35:27 +0400

    commit

---
+++ app/src/main/java/com/example/kolyannow/myapplication/KursActivity.java
@@ -0,0 +1,195 @@
+package com.example.kolyannow.myapplication;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class KursActivity extends Activity {
+
+
+    AutoCompleteTextView autoCompleteTextView;
+    ListView listView;
+    ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
+    ArrayList<String> pair_list = new ArrayList<>();
+    Button add;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kurs);
+        new ParseKurs().execute();
+
+        autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
+        listView = findViewById(R.id.list_viev);
+        add = findViewById(R.id.button5);
+
+        SimpleAdapter adapter = new SimpleAdapter(this, arrayList, android.R.layout.simple_list_item_2,
+                new String[]{"Пара", "Курс"},
+                new int[]{R.id.pair, R.id.kurs});
+        listView.setAdapter(adapter);
+
+
+
+    }
+
+    public void addPair(View view){
+        pair_list.add(autoCompleteTextView.getText().toString());
+        refrashe();
+    }
+
+    public void refrashe(){
+        for (String pair: pair_list) {
+            new ParseKursPair().execute(pair);
+        }
+    }
+
+
+    private class ParseKursPair extends AsyncTask<String, Void, String> {
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String resultJson = "";
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+            // получаем данные с внешнего ресурса
+            try {
+                String urrrl = "https://yobit.net/api/3/ticker/"+strings[0];
+                URL url = new URL(urrrl);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+
+                resultJson = buffer.toString();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return resultJson;
+        }
+
+
+        @Override
+        protected void onPostExecute(String strJson) {
+            super.onPostExecute(strJson);
+            // выводим целиком полученную json-строку
+
+//            Log.d(LOG_TAG, strJson);
+
+            JSONObject dataJsonObj = null;
+            String secondName = "";
+
+            try {
+                dataJsonObj = new JSONObject(strJson);
+                JSONArray jsonArray = dataJsonObj.names();
+
+                // 1. достаем инфо о втором друге - индекс 1
+//                JSONObject secondFriend = dataJsonObj.getJSONObject();
+//                purchase_price.setText(secondFriend.getString("avg"));
+//                sales_price.setText(secondFriend.getString("last"));
+//                Log.d(LOG_TAG, "Второе имя: " + secondName);
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class ParseKurs extends AsyncTask<Void, Void, String> {
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String resultJson = "";
+
+        @Override
+        protected String doInBackground(Void... params) {
+            // получаем данные с внешнего ресурса
+            try {
+                URL url = new URL("https://yobit.net/api/3/info");
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+
+                resultJson = buffer.toString();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return resultJson;
+        }
+
+        @Override
+        protected void onPostExecute(String strJson) {
+            super.onPostExecute(strJson);
+            // выводим целиком полученную json-строку
+
+//            Log.d(LOG_TAG, strJson);
+
+            JSONObject dataJsonObj = null;
+            ArrayList<String> list = new ArrayList<>();
+
+            try {
+                dataJsonObj = new JSONObject(strJson);
+                JSONObject pair = dataJsonObj.getJSONObject("pairs");
+                JSONArray pairs = pair.names();
+
+                for (int i = 0; i < pairs.length(); i++){
+                    list.add(pairs.getString(i));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(KursActivity.this, R.layout.support_simple_spinner_dropdown_item, list);
+            autoCompleteTextView.setAdapter(adapter);
+        }
+    }
+}
